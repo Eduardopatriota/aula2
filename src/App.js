9ce44e4f04854931488f26478e79dc62375c34c6
Eduardopatriota <39865404+Eduardopatriota@users.noreply.github.com>
2019-11-02 11:04:33 -0300

    commit pq o james quer

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Olá mundo </h1>
+      <h2> teste2 </h2>
+      <li>
+        quem é James 
+        <li> o James é...</li>
+        
+
+      </li>
+    </div>
+  );
+}
+
+export default App;
